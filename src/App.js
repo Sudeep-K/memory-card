79d80414ca77f07242084accd9329334bbe59eec
Sudeep-K <pong32btl@gmail.com>
2023-03-24 13:31:57 +0545

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Index from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className='header'>
+        <img src='./images/title-page.png'/>
+      </div>
+      <Index />
+      <img src='./images/background.jpg' className='background-art' />
+    </React.Fragment>
   );
 }
 
